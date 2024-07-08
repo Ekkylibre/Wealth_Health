bd4f5651cb35ad8a6ab27b0bd03198a0aea75552
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-07-08 23:56:30 +0200

    ✨ Add Boostrap

--- src/pages/CreateEmployeePage.jsx
+++ src/pages/CreateEmployeePage.jsx
@@ -1,7 +1,25 @@
+import { Button, Alert, Container, Row, Col } from 'react-bootstrap';
+
 function CreateEmployeePage() {
   return (
-    <div>CreateEmployeePage</div>
-  )
+    <Container>
+      <Row className="mt-5">
+        <Col>
+          <Alert variant="success">
+            This is a success alert—check it out!
+          </Alert>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant="primary">Primary Button</Button>
+        </Col>
+        <Col>
+          <Button variant="secondary">Secondary Button</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default CreateEmployeePage
