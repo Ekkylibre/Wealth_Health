ce926f9f1062a76547da48e1960eb0b9a6c744ee
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-07-09 17:04:52 +0200

    ✨ Pages > ListEmployeePage > Add SearchBar

--- src/pages/ListEmployeePage.jsx
+++ src/pages/ListEmployeePage.jsx
@@ -21,15 +21,37 @@ const columns = [
 
 function ListEmployeePage() {
   const [employees, setEmployees] = useState([]);
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const employeesData = JSON.parse(localStorage.getItem('employees')) || [];
     setEmployees(employeesData);
+    setFilteredEmployees(employeesData); // Initialise également les employés filtrés avec toutes les données
   }, []);
 
   const handleClearLocalStorage = () => {
     localStorage.removeItem('employees');
     setEmployees([]);
+    setFilteredEmployees([]);
+  };
+
+  const handleSearchChange = (e) => {
+    const searchTerm = e.target.value;
+    setSearchTerm(searchTerm);
+
+    // Filtrer les employés en fonction du terme de recherche
+    const filteredData = employees.filter(employee =>
+      employee.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      employee.lastName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      employee.department.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      employee.street.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      employee.city.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      employee.state.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      employee.zipCode.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredEmployees(filteredData);
   };
 
   return (
@@ -37,9 +59,18 @@ function ListEmployeePage() {
       <h1>Current Employees</h1>
       <Link to="/">Home</Link>
       <button className="btn btn-danger mb-3" onClick={handleClearLocalStorage}>Clear Data</button>
+      
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+        className="form-control mb-3"
+      />
+      
       <DataTable
         columns={columns}
-        data={employees}
+        data={filteredEmployees}
         pagination
       />
     </div>
