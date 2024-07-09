934d36888175c335c257070f2b1bbba542956930
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-07-09 17:01:15 +0200

    🔥 Pages > ListEmployeePage > Rework display Date

--- src/pages/ListEmployeePage.jsx
+++ src/pages/ListEmployeePage.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react';
 import DataTable from 'react-data-table-component';
 import { Link } from 'react-router-dom';
+import { format } from 'date-fns'; // Importez la fonction format de date-fns
 
 const columns = [
   { name: 'First Name', selector: row => row.firstName, sortable: true },
   { name: 'Last Name', selector: row => row.lastName, sortable: true },
-  { name: 'Start Date', selector: row => row.startDate, sortable: true },
+  { name: 'Start Date', 
+    selector: row => format(new Date(row.startDate), 'dd/MM/yyyy'), 
+    sortable: true },
   { name: 'Department', selector: row => row.department, sortable: true },
-  { name: 'Date of Birth', selector: row => row.dateOfBirth, sortable: true },
+  { name: 'Date of Birth', 
+    selector: row => format(new Date(row.dateOfBirth), 'dd/MM/yyyy'), 
+    sortable: true },
   { name: 'Street', selector: row => row.street, sortable: true },
   { name: 'City', selector: row => row.city, sortable: true },
   { name: 'State', selector: row => row.state, sortable: true },
@@ -23,9 +28,7 @@ function ListEmployeePage() {
   }, []);
 
   const handleClearLocalStorage = () => {
-    // Efface les données de localStorage
     localStorage.removeItem('employees');
-    // Réinitialise les employés à un tableau vide
     setEmployees([]);
   };
 
