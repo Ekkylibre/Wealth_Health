dce3ce4590a07b98226b019bea0b3eac7a18f1fa
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-07-11 16:46:26 +0200

    ⚡️ Work on SEO & Performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
-import CreateEmployeePage from "./pages/CreateEmployeePage";
-import ListEmployeePage from "./pages/ListEmployeePage";
-import Error from "./pages/Error";
+import { Suspense, lazy } from "react";
+
+const CreateEmployeePage = lazy(() => import("./pages/CreateEmployeePage"));
+const ListEmployeePage = lazy(() => import("./pages/ListEmployeePage"));
+const Error = lazy(() => import("./pages/Error"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<CreateEmployeePage />} />
-      <Route path="/ListEmployeePage" element={<ListEmployeePage />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<CreateEmployeePage />} />
+        <Route path="/ListEmployeePage" element={<ListEmployeePage />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Suspense>
   );
 }
 
